ae359e20e259c39c8ae26fbf017002bf6f908a73
Nils ten Hoeve <ntenhoeve@meyn.com>
2026-05-19 12:49:43 +0200

    Fixed #110

--- lib/meyncraft/tab/tab.presentation.dart
+++ lib/meyncraft/tab/tab.presentation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:meyncraft/meyncraft/command.domain.dart';
 import 'package:meyncraft/meyncraft/tab/tab.service.dart';
 
 abstract class ClosableTab extends StatefulWidget {
@@ -19,6 +20,13 @@ class ClosableTabsView extends StatefulWidget {
 class _ClosableTabsViewState extends State<ClosableTabsView> {
   final TabService _tabService = GetIt.I.get<TabService>();
 
+  final toolTipText = [
+    SelectNextTab().descriptionWithHotkey,
+    SelectPreviousTab().descriptionWithHotkey,
+    CloseCurrentTab().descriptionWithHotkey,
+    CloseAllTabs().descriptionWithHotkey,
+  ].join('\n');
+
   @override
   Widget build(BuildContext context) => ListenableBuilder(
     listenable: _tabService,
@@ -30,45 +38,48 @@ class _ClosableTabsViewState extends State<ClosableTabsView> {
       return Column(
         children: [
           // Tabs header
-          Container(
-            height: 40,
-            color: Theme.of(context).colorScheme.surfaceDim,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: _tabService.tabs.length,
-              itemBuilder: (context, index) {
-                final tab = _tabService.tabs[index];
-                final selected = index == _tabService.selectedIndex;
+          Tooltip(
+            message: toolTipText,
+            child: Container(
+              height: 40,
+              color: Theme.of(context).colorScheme.surfaceDim,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: _tabService.tabs.length,
+                itemBuilder: (context, index) {
+                  final tab = _tabService.tabs[index];
+                  final selected = index == _tabService.selectedIndex;
 
-                return InkWell(
-                  onTap: () => setState(() => _tabService.selectTab(index)),
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 12),
-                    decoration: BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          width: 2,
-                          color: selected
-                              ? Theme.of(context).colorScheme.primary
-                              : Colors.transparent,
+                  return InkWell(
+                    onTap: () => setState(() => _tabService.selectTab(index)),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            width: 2,
+                            color: selected
+                                ? Theme.of(context).colorScheme.primary
+                                : Colors.transparent,
+                          ),
                         ),
                       ),
-                    ),
-                    child: Row(
-                      children: [
-                        Text(tab.tabTitle),
-                        if (tab.closable) ...[
-                          const SizedBox(width: 6),
-                          GestureDetector(
-                            onTap: () => _tabService.closeTabIndex(index),
-                            child: const Icon(Icons.close, size: 16),
-                          ),
+                      child: Row(
+                        children: [
+                          Text(tab.tabTitle),
+                          if (tab.closable) ...[
+                            const SizedBox(width: 6),
+                            GestureDetector(
+                              onTap: () => _tabService.closeTabIndex(index),
+                              child: const Icon(Icons.close, size: 16),
+                            ),
+                          ],
                         ],
-                      ],
+                      ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
           ),
 
